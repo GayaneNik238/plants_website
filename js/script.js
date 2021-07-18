c668a676001d4e5e9fb79d7d5b1f9e920f32301b
GayaneNik238 <gnikoghosyan98@gmail.com>
2021-07-18 19:18:58 +0400

    added popup

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let burger = document.querySelector(".burger");
 let menu = document.querySelector(".menu");
 let ul = menu.querySelector("ul");
 let lis = ul.querySelectorAll("li");
-let icons = menu.querySelector(".icons")
+let icons = menu.querySelector(".icons");
 
 burger.addEventListener("click", function (e) {
     menu.classList.toggle("burgerMenu");
@@ -14,8 +14,8 @@ burger.addEventListener("click", function (e) {
 
 });
 
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
 window.addEventListener("resize", function () {
     if (vw > 800) {
@@ -30,8 +30,32 @@ window.addEventListener("resize", function () {
     }
 });
 
-document.getElementById('gCard-value').addEventListener('change', getValue);
-function getValue() {
-    let selectedValue = document.getElementById('gCard-value').value;
-    document.getElementById('gift-card-value').innerHTML = selectedValue;
+
+let loginBtns = document.querySelectorAll(".login-btn");
+if(loginBtns[0] || loginBtns[1]) {
+    loginBtns[0].addEventListener("click", showHideLogin);
+    loginBtns[1].addEventListener("click", showHideLogin);
 }
+
+function showHideLogin() {
+    let loginBox = document.querySelector("#login-box")
+    if(loginBox.style.display === "none" || loginBox.style.display === "") {
+        loginBox.style.display = "flex";
+        loginBox.style.flexDirection = "column"
+    } else {
+        loginBox.style.display = "none";
+    }
+}
+
+
+let gCardValue = document.getElementById("gCard-value");
+if(gCardValue) {
+    document.getElementById("gCard-value").addEventListener("change", getValue);
+
+}
+
+
+function getValue() {
+    let selectedValue = document.getElementById("gCard-value").value;
+    document.getElementById("gift-card-value").innerHTML = selectedValue;
+};
